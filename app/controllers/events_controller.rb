0e07e3b6b1cb4f2b8597ae89cca96f94e0f23fbe
ketan <ketan01@gmail.com>
2024-06-13 18:48:38 +0530

    Implement create action for the event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,26 @@
 class EventsController < ApplicationController
   before_action :set_current_user, if: :user_signed_in?, only: [:index]
+  before_action :set_current_user, only: [:create]
   def index
     @events = Event.all
+  end
+
+  def create
+    @event = @current_user.created_events.new(event_params)
+    if @event.save
+      redirect_to user_path(@current_user)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_current_user
     @current_user = current_user
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :description, :event_date).merge(creator_id: @current_user.id)    
   end
 end
