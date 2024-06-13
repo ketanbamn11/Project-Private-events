76e7df2ebbaf2181f8a8197276fdb8cbd4ed8983
ketan <ketan01@gmail.com>
2024-06-13 16:43:03 +0530

    Add the association between the event and event creator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_13_103829) do
+ActiveRecord::Schema.define(version: 2024_06_13_110218) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2024_06_13_103829) do
     t.date "event_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
