3ebd803f7a1549f1a58fac08a3a51bf775cb4f52
ketan <ketan01@gmail.com>
2024-06-13 22:41:49 +0530

    Create and migrate tables, and define foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_13_110218) do
+ActiveRecord::Schema.define(version: 2024_06_13_162737) do
+
+  create_table "event_attendees", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "event_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_event_attendees_on_event_id"
+    t.index ["user_id"], name: "index_event_attendees_on_user_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -34,4 +43,6 @@ ActiveRecord::Schema.define(version: 2024_06_13_110218) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "event_attendees", "events"
+  add_foreign_key "event_attendees", "users"
 end
