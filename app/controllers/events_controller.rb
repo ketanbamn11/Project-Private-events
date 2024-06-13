8d4a9ae8b30307a2c62eaf8fe125abbba2265f2e
ketan <ketan01@gmail.com>
2024-06-13 17:35:37 +0530

    Create a page that shows all the events the user has made

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,12 @@
 class EventsController < ApplicationController
+  before_action :set_current_user, if: :user_signed_in?, only: [:index]
   def index
     @events = Event.all
+  end
+
+  private
+
+  def set_current_user
+    @current_user = current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_scope :user do  
     get '/users/sign_out' => 'devise/sessions#destroy'     
   end
+  resources :users
   # delete 'users/sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
