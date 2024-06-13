69be82a5b0ba1ed62ec6aefed686050a541bde9d
ketan <ketan01@gmail.com>
2024-06-13 16:26:31 +0530

    Set up user model and authenticate via devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "events#index"
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+  # delete 'users/sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
