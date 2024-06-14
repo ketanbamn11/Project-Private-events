0148c54e02e39f3a74c1d854585cc2b4464754b8
ketan <ketan01@gmail.com>
2024-06-14 14:56:38 +0530

    Add action to leave event

--- app/controllers/event_attendees_controller.rb
+++ app/controllers/event_attendees_controller.rb
@@ -11,4 +11,15 @@ class EventAttendeesController < ApplicationController
       redirect_to @event, alert: 'There was an error joining the event.'
     end
   end
+
+  def destroy
+    @event = Event.find(params[:event_id])
+    @event_attendee = @event.event_attendances.find_by(user: current_user)
+
+    if @event_attendee&.destroy
+      redirect_to @event, notice: 'You have successfully left the event.'
+    else
+      redirect_to @event, alert: 'There was an error leaving the event.'
+    end
+  end
 end
